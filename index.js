f85cf9c20db6336bec02f55fb1631ecce0537e5d
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-17 21:37:27 +0300

    3rd attempt, index.js file

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   fetch('https://anapioficeandfire.com/api/books')
   .then((response) => response.json())
-  .then((data) => {
-    const books=data;
-  })
-  return books;
+  .then((data) => renderBooks(data))
+  
 }
 
 function renderBooks(books) {
